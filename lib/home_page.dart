3fa2f60e07e616c1cd7b3d38ad610a86195fa013
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-02-06 23:08:49 +0530

    home page started

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,9 +5,60 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Home"),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 20.0, left: 25.0, right: 25.0),
+            child: Card(
+              color: Color(0xFFFFFFFF),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    for (int number = 14; number <= 18; number++)
+                      Text(
+                        '$number',
+                        style: TextStyle(
+                          color: number == 16 ? Colors.black : Colors.grey,
+                          fontSize: 37.0,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 20.0, left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Today',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.add_outlined,
+                    color: Colors.white,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: EdgeInsets.all(20),
+                      backgroundColor: Colors.black),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
